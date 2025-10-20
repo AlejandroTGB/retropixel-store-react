561d5834e1a94e11007932600b42ad12322dd989
Alejandro Torres <alej.torresg@duocuc.cl>
2025-10-20 20:43:15 -0300

    Ajuste navbar (nosotros movido a footer y agregado vista admin)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,11 +17,11 @@ export default function Navbar() {
           <ul className={styles.navList}>
             <li><Link to="/" className={isActive('/')}>Home</Link></li>
             <li><Link to="/productos" className={isActive('/productos')}>Juegos</Link></li>
-            <li><Link to="/nosotros" className={isActive('/nosotros')}>Nosotros</Link></li>
             <li><Link to="/blogs" className={isActive('/blogs')}>Blog</Link></li>
             <li><Link to="/contacto" className={isActive('/contacto')}>Contacto</Link></li>
             <li><Link to="/login" className={isActive('/login')}>Login</Link></li>
             <li><Link to="/registro" className={isActive('/registro')}>Registro</Link></li>
+            <li><Link to="/admin" className={isActive('/admin')}>Admin</Link></li>
             <li><Link to="/carrito" className={isActive('/carrito')}>🛒</Link></li>
           </ul>
         </nav>
