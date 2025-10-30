dfa828f4c4b17cc0c725c810d7ed4ac289264e35
Alejandro Torres <alej.torresg@duocuc.cl>
2025-10-30 14:41:20 -0300

    Test unitario de CarritoContext ( agrega un producto al carrito)

---
+++ src/__tests__/unitarios/CarritoContext.unit.test.js
@@ -0,0 +1,18 @@
+import { renderHook, act } from "@testing-library/react";
+import { CarritoProvider, useCarrito } from "../../context/CarritoContext";
+
+describe('CarritoContext', () => {
+  test('agrega un producto al carrito', () => {
+    const wrapper = ({ children }) => <CarritoProvider>{children}</CarritoProvider>;
+    const { result } = renderHook(() => useCarrito(), { wrapper });
+
+    const producto = { id: 1, nombre: 'Producto Test', precio: 100 };
+
+    act(() => {
+      result.current.agregarProducto(producto);
+    });
+
+    expect(result.current.carrito).toHaveLength(1);
+    expect(result.current.carrito[0]).toMatchObject(producto);
+  });
+});
