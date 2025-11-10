66bba73f99d020683b1026480df953eb72554fde
Alejandro Torres <alej.torresg@duocuc.cl>
2025-11-10 17:28:53 -0300

    Añade el servicio de carrito con métodos para obtener, agregar, actualizar, eliminar y vaciar productos.

---
+++ src/services/carritoService.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { authService } from './authService';
+
+const API_BASE_URL = 'http://localhost:8080/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+api.interceptors.request.use((config) => {
+  const token = authService.getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const carritoService = {
+  obtenerCarrito: async () => {
+    const response = await api.get('/carrito/mi-carrito');
+    return response.data;
+  },
+
+  agregarProducto: async (productoId, cantidad) => {
+    const response = await api.post('/carrito/agregar', {
+      productoId,
+      cantidad
+    });
+    return response.data;
+  },
+
+  actualizarProducto: async (productoId, cantidad) => {
+    const response = await api.put(`/carrito/actualizar/${productoId}`, {
+      cantidad
+    });
+    return response.data;
+  },
+
+  eliminarProducto: async (productoId) => {
+    const response = await api.delete(`/carrito/eliminar/${productoId}`);
+    return response.data;
+  },
+
+  vaciarCarrito: async () => {
+    const response = await api.delete('/carrito/vaciar');
+    return response.data;
+  }
+};
