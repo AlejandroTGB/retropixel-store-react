fc52cd54f01d7d2c27d56278175b9ba557f75905
Alejandro Torres <alej.torresg@duocuc.cl>
2025-12-17 12:22:44 -0300

    Agregados metodos obtenerTodos y actualizarEstado

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import { authService } from './authService';
+import axios from "axios";
+import { authService } from "./authService";
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
@@ -20,17 +20,27 @@ api.interceptors.request.use((config) => {
 
 export const pedidoService = {
   crearPedido: async (email, nombre, metodoPago) => {
-    const response = await api.post('/pedidos/crear', {
+    const response = await api.post("/pedidos/crear", {
       email,
       nombre,
       metodoPago,
-      estado: 'pagado'
+      estado: "pagado",
     });
     return response.data;
   },
 
   obtenerMisPedidos: async () => {
-    const response = await api.get('/pedidos/mis-pedidos');
+    const response = await api.get("/pedidos/mis-pedidos");
     return response.data;
-  }
+  },
+
+  obtenerTodos: async () => {
+    const response = await api.get("/pedidos/admin/todos");
+    return response.data;
+  },
+
+  actualizarEstado: async (id, estado) => {
+    const response = await api.put(`/pedidos/admin/${id}/estado`, { estado });
+    return response.data;
+  },
 };
